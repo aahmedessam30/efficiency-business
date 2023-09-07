7088a85842b8e9f169c342b6ebc1968220d79d6f
Ahmed Essam <aahmedessam30@gmail.com>
2023-09-07 14:13:13 +0300

    Add Inertia Js

---
+++ resources/js/Pages/Welcome/index.vue
@@ -0,0 +1,32 @@
+<template>
+    <Layout>
+        <h1>Welcome, {{ name }}</h1>
+    </Layout>
+</template>
+
+<script>
+import Layout from "../../layouts/App/index.vue";
+
+export default {
+    name: "Welcome",
+    props: {
+        name: {
+            type: String,
+            required: true,
+        },
+    },
+    components: {
+        Layout,
+    },
+    data() {
+        return {
+            //
+        }
+    },
+    methods: {
+        //
+    },
+}
+</script>
+
+<style scoped lang="scss" src="./_index.scss"/>
---
+++ resources/js/layouts/App/index.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <Header/>
+        <slot/>
+        <Footer/>
+    </div>
+</template>
+
+<script>
+import Header from "../../includes/Header/index.vue";
+import Footer from "../../includes/Footer/index.vue";
+
+export default {
+    name: "Layout",
+    components: {
+        Header,
+        Footer,
+    },
+}
+</script>
+
+<style scoped lang="scss" src="./_index.scss"/>
