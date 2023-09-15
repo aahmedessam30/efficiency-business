2c28bf54b86bca727d4bb1ac06aedb75753db40c
Ahmed Essam <aahmedessam30@gmail.com>
2023-09-15 03:27:09 +0300

    Add About Us Section

--- lang/en/front.php
+++ lang/en/front.php
@@ -6,4 +6,8 @@ return [
     'services' => 'Our Services',
     'our_clients' => 'Our Clients',
     'contact' => 'Contact Us',
+    'about_us_title' => 'We Are Advertising Agency',
+    'about_us_description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Dictumst habitant viverra placerat volutpat adipiscing urna risus, a turpis. Orci, aliquam in id etiam praesent scelerisque leo. Nec blandit tellus congue faucibus nulla viverra aliquet ullamcorper in. Non viverra ornare id sed massa ut eu. Viverra facilisis volutpat elit magnis
+
+Volutpat tristique sapien aliquet in lacinia. Porttitor placerat molestie odio at nisl venenatis mauris senectus varius. In aliquam augue elementum porttitor amet, elementum. Tortor nulla gravida condimentum leo, sit amet, mus. Nec in nec sollicitudin nullam platea.',
 ];
--- resources/js/components/About/index.vue
+++ resources/js/components/About/index.vue
@@ -1,9 +1,15 @@
 <template>
     <div class="about">
-        <div class="container">
-            <div class="row">
-                <div class="col-12">
-                    <h1 class="about__title">About</h1>
+        <div class="container mx-auto py-40 px-5 w-full">
+            <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-2 gap-20">
+                <div
+                    class="flex items-center justify-center bg-cover bg-center h-[700px] w-full rounded-[25px] shadow-lg transform hover:scale-105 transition duration-500 ease-in-out"
+                :class="`bg-[url('https://picsum.photos/400/400')]`">
+                </div>
+                <div class="flex flex-col justify-center">
+                    <h1 class="text-sm font-serif font-bold pb-2 text-[20px]">{{ $t('front.about') }}</h1>
+                    <h2 class="text-6xl font-serif font-bold pb-10">{{ $t('front.about_us_title') }}</h2>
+                    <p class="text-lg">{{ $t('front.about_us_description') }}</p>
                 </div>
             </div>
         </div>
