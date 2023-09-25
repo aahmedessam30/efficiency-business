29f092b16823cfc0e120bd947b1dafbd5618a1d9
Ahmed Essam <aahmedessam30@gmail.com>
2023-09-26 02:28:23 +0300

    Update index.vue

--- resources/js/components/WhyChooseUs/index.vue
+++ resources/js/components/WhyChooseUs/index.vue
@@ -8,7 +8,7 @@
         <div class="grid grid-cols-1 md:grid-cols-3 gap-20 mt-32">
             <div class="bg-primary shadow-lg rounded-[40px] px-10 py-10 border-4 border-secondary">
                 <div class="flex justify-center">
-                    <img src="/images/why-choose-us/1.png" alt="1" class="w-20 h-20">
+                    <img src="https://picsum.photos/400/400" alt="1" class="w-20 h-20">
                 </div>
                 <h1 class="text-2xl font-bold text-white mt-5">Best Quality</h1>
                 <p class="text-white mt-5">
@@ -17,7 +17,7 @@
             </div>
             <div class="bg-primary shadow-lg rounded-[40px] px-10 pt-10 border-4 border-secondary -mt-20">
                 <div class="flex justify-center">
-                    <img src="/images/why-choose-us/2.png" alt="1" class="w-20 h-20">
+                    <img src="https://picsum.photos/400/400" alt="1" class="w-20 h-20">
                 </div>
                 <h1 class="text-2xl font-bold text-white mt-5">Best Quality</h1>
                 <p class="text-white mt-5">
@@ -26,7 +26,7 @@
             </div>
             <div class="bg-primary shadow-lg rounded-[40px] px-10 py-10 border-4 border-secondary">
                 <div class="flex justify-center">
-                    <img src="/images/why-choose-us/3.png" alt="1" class="w-20 h-20">
+                    <img src="https://picsum.photos/400/400" alt="1" class="w-20 h-20">
                 </div>
                 <h1 class="text-2xl font-bold text-white mt-5">Best Quality</h1>
                 <p class="text-white mt-5">
